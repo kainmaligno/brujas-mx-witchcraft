2dd203fdb1bca7cb01678e2ac7bd5a1f624a2004
salemm <emmanuel@bquate.com>
2019-08-08 00:07:41 -0500

    :poop: on footer structure need to be :bug:

---
+++ src/device.js
@@ -0,0 +1,18 @@
+
+const size = {
+    mobileS: "320",
+    mobileM: "375",
+    mobileL: "425",
+    tablet: "768",
+    laptop: "1024",
+    laptopL: "1440",
+    desktop: "2560"
+}
+
+const device = Object.keys(size).reduce((acc, cur)=>{
+    acc[cur] = `(min-width: ${size[cur]}px)`;
+    console.log('medidas',acc[cur])
+    return acc
+},{})
+
+export default device
